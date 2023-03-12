305b955e28e1a9b529ce0d38122b16ec0e8f6764
shi <shishaopeng@gohigh.com.cn>
2023-03-12 15:16:06 +0800

    deploy,

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -201,7 +201,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
     // 页脚信息
     footer: {
-      createYear: 2019, // 博客创建年份
+      createYear: 2022, // 博客创建年份
       copyrightInfo:
         'Evan Xu | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息、备案信息等，支持a标签或换行标签</br>
     },
